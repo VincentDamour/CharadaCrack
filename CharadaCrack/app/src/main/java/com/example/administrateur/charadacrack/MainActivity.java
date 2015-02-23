76216a9c7060a8fe92a01c3231d23b4605cdf2f3
funny-vince <funny-vince@hotmail.com>
2015-02-23 17:34:46 -0500

    commit fleche recule + points + temps + next charade

--- CharadaCrack/app/src/main/java/com/example/administrateur/charadacrack/MainActivity.java
+++ CharadaCrack/app/src/main/java/com/example/administrateur/charadacrack/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 
@@ -14,6 +15,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 
